10a84b18f5ed0712f66b7fd0c3ae85e1f17625a7
ResCode75 <rachel.emily002@gmail.com>
2025-02-21 18:55:13 -0500

    please work

--- Module 10/shaw10_2.js
+++ Module 10/shaw10_2.js
@@ -3,12 +3,12 @@ function changeImage(){
 
     console.log(image1.src);
 
-    if (image1.src == "mod10images//UndertaleHeart.jpeg"){
+    if (image1.src = ("https://rescode75.github.io/csd-340/Module%2010/mod10images//UndertaleHeart.jpeg")){
         image1.src = "mod10images//UndertaleHeartbreak.jpg";
     }
     else{
         console.log(image1.src);
-        image1.src = "mod10images//UndertaleHeart.jpeg";
+        image1.src = "https://rescode75.github.io/csd-340/Module%2010/mod10images/UndertaleHeart.jpeg";
     }
 
     
