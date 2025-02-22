3b645add411ee22366077ed346a4e500cd51a1c1
ResCode75 <rachel.emily002@gmail.com>
2025-02-21 19:23:19 -0500

    10.3 assignment

--- Module 10/shaw10_2.js
+++ Module 10/shaw10_2.js
@@ -1,9 +1,8 @@
 function changeImage(){
     var image1 = document.getElementById("changeable1");
-
     console.log(image1.src);
 
-    if (image1.src = ("https://rescode75.github.io/csd-340/Module%2010/mod10images//UndertaleHeart.jpeg")){
+    if (image1.src == "https://rescode75.github.io/csd-340/Module%2010/mod10images/UndertaleHeart.jpeg"){
         image1.src = "mod10images//UndertaleHeartbreak.jpg";
     }
     else{
@@ -13,3 +12,4 @@ function changeImage(){
 
     
 } 
+
