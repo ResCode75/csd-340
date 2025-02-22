e2cf519f9bd4b6e65aaece0983a9e4379770663c
ResCode75 <rachel.emily002@gmail.com>
2025-02-21 19:23:48 -0500

    module 10.3 assignment 2/21/2025

--- Module 10/shaw10_2.js
+++ Module 10/shaw10_2.js
@@ -1,7 +1,7 @@
+//Rachel Shaw - module 10.3 assignment - 2/21/2025
 function changeImage(){
     var image1 = document.getElementById("changeable1");
-    console.log(image1.src);
-
+    //check source of image 1, change if it is the broken heart image
     if (image1.src == "https://rescode75.github.io/csd-340/Module%2010/mod10images/UndertaleHeart.jpeg"){
         image1.src = "mod10images//UndertaleHeartbreak.jpg";
     }
